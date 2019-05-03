4c7afb17e9bd28c56c7b3f2209982438605ec152
Evan <ebrook3@gatech.edu>
2019-05-03 01:42:08 -0400

    message boi

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+    public GameObject player2;
+
+    Camera cam;
+
+    Vector3 playerCam;
+    Vector3 player2Cam;
+
+    float height;
+    float moveHorizontal;
+
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+        cam = GetComponent<Camera>();
+        playerCam = cam.WorldToScreenPoint(player.transform.position);
+        player2Cam = cam.WorldToScreenPoint(player2.transform.position);
+        height = cam.pixelHeight;
+    }
+    private void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        playerCam = cam.WorldToScreenPoint(player.transform.position);
+        player2Cam = cam.WorldToScreenPoint(player2.transform.position);
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        float playerAvg = (playerCam.x + player2Cam.x) / 2;
+        Vector3 camDif = new Vector3(0, 0, 0);
+        Vector3 camLeft = new Vector3(0, 0, 0);
+        camLeft = cam.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        if (moveHorizontal > 0)
+        {
+            if (playerAvg > cam.pixelWidth / 2)
+            {
+                camDif = cam.ScreenToWorldPoint(new Vector3(playerAvg, height / 2));
+                transform.position = camDif;
+            }
+        } else if (moveHorizontal < 0 && camLeft.x >= -18)
+        {
+            if (playerAvg < cam.pixelWidth / 2)
+            {
+                camDif = cam.ScreenToWorldPoint(new Vector3(playerAvg, height / 2));
+                transform.position = camDif;
+            }
+        }
+        //transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/HorizontalMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour {
+    public float speed;
+    public Camera cam;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position + new Vector3(movement.x * speed, movement.y * speed), 0.01f);
+        Collider2D[] collider2 = Physics2D.OverlapCircleAll(transform.position + new Vector3(movement.x * speed, movement.y * speed + 0.5f), 0.01f);
+        Collider2D[] collider3 = Physics2D.OverlapCircleAll(transform.position + new Vector3(movement.x * speed, movement.y * speed - 0.48f), 0.01f);
+        if (moveHorizontal > 0)
+        {
+            collider = Physics2D.OverlapCircleAll(transform.position + new Vector3(movement.x * speed + 0.5f, movement.y * speed), 0.01f);
+            collider2 = Physics2D.OverlapCircleAll(transform.position + new Vector3(movement.x * speed + 0.5f, movement.y * speed + 0.5f), 0.01f);
+            collider3 = Physics2D.OverlapCircleAll(transform.position + new Vector3(movement.x * speed + 0.5f, movement.y * speed - 0.48f), 0.01f);
+        } else if (moveHorizontal < 0)
+        {
+            collider = Physics2D.OverlapCircleAll(transform.position + new Vector3(movement.x * speed - 0.5f, movement.y * speed), 0.01f);
+            collider2 = Physics2D.OverlapCircleAll(transform.position + new Vector3(movement.x * speed - 0.5f, movement.y * speed + 0.5f), 0.01f);
+            collider3 = Physics2D.OverlapCircleAll(transform.position + new Vector3(movement.x * speed - 0.5f, movement.y * speed - 0.48f), 0.01f);
+        }
+        bool col = false;
+        foreach (var collide in collider)
+        {
+            if (collide.tag != "Player")
+            {
+                if (collide.tag != "Ground")
+                {
+                    //Debug.Log(collide);
+                    col = true;
+                }
+            }
+        }
+        foreach (var collide2 in collider2)
+        {
+            if (collide2.tag != "Player" && collide2.tag != "Ground")
+            {
+                col = true;
+            }
+        }
+        foreach (var collide3 in collider3)
+        {
+            if (collide3.tag != "Player" && collide3.tag != "Ground")
+            {
+                //Debug.Log(collide);
+                col = true;
+            }
+        }
+        if ((transform.position.x + movement.x * speed) <= -18)
+        {
+            col = true;
+            transform.position = new Vector3(-18, transform.position.y);
+        }
+        if (col == false)
+        {
+            if (cam.WorldToScreenPoint(transform.position).x <= cam.pixelWidth - 20 && moveHorizontal > 0)
+            {
+                transform.position = transform.position + new Vector3(movement.x * speed, movement.y * speed);
+            } else if (cam.WorldToScreenPoint(transform.position).x >= 20 && moveHorizontal < 0)
+            {
+                transform.position = transform.position + new Vector3(movement.x * speed, movement.y * speed);
+            }
+        }
+        //transform.position = transform.position + new Vector3(movement.x * speed, movement.y * speed);
+    }
+}
