f3d9851bbc158b9acbcc1f95920942e1cb6f58a5
Sean M McManus <theprefontainemustache@hotmail.com>
2019-06-10 19:04:06 -0400

    a simple health system and simple enemy ai
    This will get more advanced later on but this will do for now

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth;
+    int curHealth;
+    bool dead;
+    public float invuln;
+    bool damageable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+        dead = false;
+        damageable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the invulnerability timeframe has ended, allow the target to me hittable again
+        if(!damageable && invuln < Time.time)
+        {
+            damageable = true;
+        }
+    }
+
+    public void takeDamage(int damage)
+    {
+        //target is possible to take damage
+        if (damageable)
+        {
+            invuln = Time.time + 2f;
+            curHealth -= damage;
+            Debug.Log(gameObject.name + " has taken damage");
+            Debug.Log("Current Health: " + curHealth);
+            //if the target's health goes down to zero, kill the target. currently a placeholder
+            if (curHealth <= 0)
+            {
+                Debug.Log(gameObject.name + " has died");
+                dead = true;
+            }
+        }
+    }
+
+    //Not sure if this will be needed or not
+    public bool getDead()
+    {
+        return dead;
+    }
+
+    //same as above
+    public int getCurHealth()
+    {
+        return curHealth;
+    }
+
+}
