d27e85c45edb612cc52482e41cf2af7c734d28cf
Evan <ebrook3@gatech.edu>
2019-07-20 20:10:35 -0400

    minor changes and level goal

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public int curLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if (curLevel == 1)
+            {
+                SceneManager.LoadScene("Level 2");
+            }
+        }
+    }
+}
