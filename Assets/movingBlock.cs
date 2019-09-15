9a088fad9c07e02f923d976649789851a72e5299
Evan <ebrook3@gatech.edu>
2019-09-15 16:23:57 -0400

    slight level edits

---
+++ Assets/movingBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingBlock : MonoBehaviour
+{
+    public Vector3 speed;
+    public int time;
+    int timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer != time)
+        {
+            timer++;
+            transform.position += speed;
+        }
+        else {
+            timer = 0;
+            speed.x = speed.x * -1;
+        }
+    }
+}
