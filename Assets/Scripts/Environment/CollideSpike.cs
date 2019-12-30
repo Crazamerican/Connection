f4fa06973cce94338a5393df7e399ed3632ae1d1
Evan <ebrook3@gatech.edu>
2019-12-29 20:55:29 -0500

    small changes

--- Assets/Scripts/Environment/CollideSpike.cs
+++ Assets/Scripts/Environment/CollideSpike.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class CollideSpike : MonoBehaviour
 {
     DeathScript deathScript;
+    public GameObject playerThings;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        deathScript = playerThings.GetComponent<DeathScript>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
