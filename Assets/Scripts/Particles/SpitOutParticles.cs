6e0eafe74a4429f9902db308b438cdb2a00765bd
Kevin Tang <kev123361@gmail.com>
2020-04-03 18:02:29 -0400

    particle rain prefab

--- Assets/Scripts/Particles/SpitOutParticles.cs
+++ Assets/Scripts/Particles/SpitOutParticles.cs
@@ -6,8 +6,11 @@ public class SpitOutParticles : MonoBehaviour
 {
     public Rigidbody2D particle;
 
-    //time between generating particles
+    [Header("time between generating particles")]
     public float generationRate;
+
+    [Header("horizontal distance to generate particles")]
+    public float width = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -25,10 +28,12 @@ public class SpitOutParticles : MonoBehaviour
     {
         while (true)
         {
-            Rigidbody2D p = Instantiate(particle, transform.position + new Vector3(Random.Range(-20f, 20f), 0f, 0f),
+            Rigidbody2D p = Instantiate(particle, transform.position + new Vector3(Random.Range(-width, width), 0f, 0f),
                 transform.rotation);
             p.velocity = new Vector3(0f, Random.Range(-1f, -.1f), 0f);
             p.angularVelocity = Random.Range(-90f, 90f);
+            Destroy(p.gameObject, 40);
+            p.transform.localScale = new Vector3(Random.Range(.7f, 1f), Random.Range(.7f, 1f));
 
             yield return new WaitForSecondsRealtime(generationRate);
         }
