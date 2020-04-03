d7e1e3ede33dd37b9cfa7355d96c87e3a7979645
Kevin Tang <kev123361@gmail.com>
2020-04-03 18:28:23 -0400

    Particle rain in new scenes

--- Assets/Scripts/Particles/SpitOutParticles.cs
+++ Assets/Scripts/Particles/SpitOutParticles.cs
@@ -12,9 +12,24 @@ public class SpitOutParticles : MonoBehaviour
     [Header("horizontal distance to generate particles")]
     public float width = 20;
 
+    [Header("number of particles in each generation")]
+    public int numBurst = 3;
+
     // Start is called before the first frame update
     void Start()
     {
+        //warmup the scene
+        for (int i = 0; i < 10; i++)
+        {
+            Rigidbody2D p = Instantiate(particle, transform.position + new Vector3(Random.Range(-width, width), 0f, 0f),
+                transform.rotation);
+            p.velocity = new Vector3(0f, Random.Range(-.6f, -.4f), 0f);
+            p.angularVelocity = Random.Range(-90f, 90f);
+            Destroy(p.gameObject, 40);
+            p.transform.localScale = new Vector3(Random.Range(.7f, 1f), Random.Range(.7f, 1f));
+        }
+
+
         StartCoroutine(GenerateParticles());
     }
 
@@ -28,12 +43,15 @@ public class SpitOutParticles : MonoBehaviour
     {
         while (true)
         {
-            Rigidbody2D p = Instantiate(particle, transform.position + new Vector3(Random.Range(-width, width), 0f, 0f),
-                transform.rotation);
-            p.velocity = new Vector3(0f, Random.Range(-1f, -.1f), 0f);
-            p.angularVelocity = Random.Range(-90f, 90f);
-            Destroy(p.gameObject, 40);
-            p.transform.localScale = new Vector3(Random.Range(.7f, 1f), Random.Range(.7f, 1f));
+            for (int i = 0; i < numBurst; i++)
+            {
+                Rigidbody2D p = Instantiate(particle, transform.position + new Vector3(Random.Range(-width, width), 0f, 0f),
+                    transform.rotation);
+                p.velocity = new Vector3(0f, Random.Range(-.6f, -.4f), 0f);
+                p.angularVelocity = Random.Range(-90f, 90f);
+                Destroy(p.gameObject, 40);
+                p.transform.localScale = new Vector3(Random.Range(.7f, 1f), Random.Range(.7f, 1f));
+            }
 
             yield return new WaitForSecondsRealtime(generationRate);
         }
