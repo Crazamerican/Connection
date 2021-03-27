68eeb63521d6d23d8f34115526e6cb8132ee922b
Evan <ebrook3@gatech.edu>
2021-03-27 10:01:54 -0400

    chchchanges laser and title screen

---
+++ Assets/MovinCloud.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovinCloud : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= -51.35759f)
+        {
+            transform.position = new Vector3(8.237969f, transform.position.y, transform.position.z);
+        }
+        else {
+            transform.position = transform.position + new Vector3(-speed, 0);
+        }
+    }
+}
