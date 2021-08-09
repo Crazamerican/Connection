d6307d22c21fd3c8d6b372c4aa2183e9187ffdde
Evan <ebrook3@gatech.edu>
2021-08-09 19:50:21 -0400

    laser update?

---
+++ Assets/LaserShotTrig.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserShotTrig : MonoBehaviour
+{
+    Animator parent_Anim;
+    Animator anim;
+    DeathScript deathScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent_Anim = this.transform.parent.gameObject.GetComponent<Animator>();
+        anim = this.GetComponent<Animator>();
+        deathScript = FindObjectOfType<DeathScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (parent_Anim.GetBool("ShootLaser")) {
+            anim.SetBool("BeginLaser", true);
+        }
+        if (parent_Anim.GetBool("CurLaser")) {
+            anim.SetBool("EndLaser", true);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && anim.GetBool("ActualLaser")) {
+            deathScript.dead = true;
+        }
+    }
+}
